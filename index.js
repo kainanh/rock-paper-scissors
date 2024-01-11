91724b1f0ce60de9622cac4b56e237060d117275
Kainan <kainanh@hotmail.co.uk>
2024-01-11 07:33:37 +0000

    Game functionality completed but no UI

---
+++ index.js
@@ -0,0 +1,29 @@
+const options = ['Rock', 'Paper', 'Scissors']
+
+function getComputerChoice(){
+    let choice = Math.floor(Math.random()*3)
+    return options[choice]
+}
+
+
+function gameRound(event){
+    let playerSelection = event.target.value
+    let computerSelection = getComputerChoice()
+
+    if(playerSelection == computerSelection){
+        console.log("Great minds think alike. This round ends in a draw")
+    } else if(playerSelection == 'Rock' && computerSelection == 'Scissors'){
+        console.log(playerSelection, computerSelection)
+        console.log("Congrats player!, You won this round")
+    } else if(playerSelection == 'Scissors' && computerSelection == 'Paper'){
+        console.log(playerSelection, computerSelection)
+        console.log("Congrats player!, You won this round")
+    } else if(playerSelection == 'Paper' && computerSelection == 'Rock'){
+        console.log(playerSelection, computerSelection)
+        console.log("Congrats player!, You won this round")
+    } else {
+        console.log(playerSelection, computerSelection)
+        console.log("Wow the computer is living in your head rent free buddy. You lost this round")
+    }
+}
+
